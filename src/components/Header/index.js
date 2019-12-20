db5becae02d4da20e0df0900b15cec3e77ff0344
Yan Oliveira <yan.brito@hotmail.com>
2019-12-20 11:55:04 -0300

    Create proptypes in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-
 import { Container, Logo, Cart, Item, LogoImg } from './styles';
 
 import logo from '../../assets/images/logo.png';
 
 function Header({ navigation, cartSize }) {
+  Header.propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+    cartSize: PropTypes.shape({}).isRequired,
+  };
   return (
     <Container>
       <Logo onPress={() => navigation.navigate('Main')}>
