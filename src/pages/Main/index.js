89cf76307f5363fc240b0c6e4f4ec3ea2caf4cc8
skulldarth <yan.brito@hotmail.com>
2019-12-29 23:56:26 -0300

    Create Main page style

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,18 +2,47 @@ import React from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container, List, Product, CartButton } from './styles';
+import {
+  Container,
+  List,
+  Product,
+  Image,
+  ProductTitle,
+  ProductPrice,
+  CartButton,
+  ItemCount,
+  ItemCart,
+  CartButtonText,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
-      <List>
-        <Product>
-          <CartButton>
-            <Icon name="add-shopping-cart" size={20} color="#fff" />
-          </CartButton>
-        </Product>
-      </List>
+      <List
+        horizontal
+        data={[
+          {
+            title: 'Tênis de Caminhada Leve Confortável',
+            img:
+              'https://static.netshoes.com.br/produtos/tenis-vr-sneaker-meia-leve/06/E74-0492-006/E74-0492-006_zoom1.jpg',
+            price: 'R$179,90',
+          },
+        ]}
+        renderItem={({ item }) => (
+          <Product>
+            <Image source={{ uri: item.img }} />
+            <ProductTitle>{item.title}</ProductTitle>
+            <ProductPrice>{item.price}</ProductPrice>
+            <CartButton>
+              <ItemCart>
+                <Icon name="add-shopping-cart" size={20} color="#fff" />
+                <ItemCount>3</ItemCount>
+              </ItemCart>
+              <CartButtonText>ADICIONAR</CartButtonText>
+            </CartButton>
+          </Product>
+        )}
+      />
     </Container>
   );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -8,23 +8,59 @@ export const Container = styled.View`
 `;
 
 export const List = styled.FlatList.attrs({
-  showsVerticalScrollIndicator: false,
+  showsHorizontalScrollIndicator: false,
 })`
   margin-top: 20px;
 `;
 
 export const Product = styled.View`
-  flex-direction: row;
   background-color: #fff;
   width: 220px;
   height: 350px;
+  justify-content: space-between;
+  padding: 10px;
+`;
+
+export const Image = styled.Image`
+  width: 200px;
+  height: 200px;
+`;
+
+export const ProductTitle = styled.Text`
+  font-size: 16px;
+  padding-left: 10px;
+`;
+
+export const ProductPrice = styled.Text`
+  font-size: 21px;
+  padding-left: 10px;
 `;
 
 export const CartButton = styled(RectButton)`
-  justify-content: center;
+  flex-direction: row;
+  justify-content: space-between;
   align-items: center;
+  width: 200px;
+  height: 42px;
   background: #7159c1;
+  color: #fff;
   border-radius: 4px;
-  margin-left: 10px;
-  padding: 0 12px;
+  margin-top: auto;
+`;
+
+export const ItemCart = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 13px;
+  background: rgba(0, 0, 0, 0.1);
+`;
+
+export const ItemCount = styled.Text`
+  font-size: 14px;
+  color: #ffffff;
+`;
+
+export const CartButtonText = styled.Text`
+  font-size: 16px;
+  padding-left: 36px;
 `;
