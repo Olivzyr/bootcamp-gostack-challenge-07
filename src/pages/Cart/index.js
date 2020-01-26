58a0642c4e128ae215e9870c36c756eddbaf80c7
skulldarth <yan.brito@hotmail.com>
2020-01-26 19:04:40 -0300

    Cart page modify data

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -14,7 +14,7 @@ export default function Cart() {
             image: '',
             title: '',
             price: '',
-            // productTotalPrice: '',
+            productTotalPrice: '',
           },
         ]}
       />
